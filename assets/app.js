3cea19209bda694a5eaa1bd934d0c69d622e742e
Tomkc518 <32521131+Tomkc518@users.noreply.github.com>
2018-04-08 18:26:06 -0500

    updating hover effects

--- assets/app.js
+++ assets/app.js
@@ -41,6 +41,6 @@ $(function() {
     $('#return-to-top').click(function() {      // When arrow is clicked
         $('body,html').animate({
             scrollTop : 0                       // Scroll to top of body
-        }, 500);
+        }, 1500);
     });
 });
